1f7cc0b5d3f9101bf9b8c9a32bc42eb3e783700f
npmcatipon <npmcatipon@bpi.com.ph>
2026-01-30 19:52:56 +0800

    added README, SQL files and pom.xml

---
+++ insert-data.sql
@@ -0,0 +1,29 @@
+INSERT INTO book (title, author, isBorrowed) VALUES
+('The Midnight Cipher', 'Evelyn Graves', false);
+
+INSERT INTO book (title, author, isBorrowed) VALUES
+('Whispers in the Manor', 'Laurence Blackwood', false);
+
+INSERT INTO book (title, author, isBorrowed) VALUES
+('The Vanishing of Hollow Creek', 'Mara Ellison', false);
+
+INSERT INTO book (title, author, isBorrowed) VALUES
+('A Shadow Behind the Door', 'J.T. Rowland', false);
+
+INSERT INTO book (title, author, isBorrowed) VALUES
+('The Clocktower Murmurs', 'Harriet Voss', false);
+
+INSERT INTO book (title, author, isBorrowed) VALUES
+('Dead Letters in Briar Lane', 'Simon R. Caldwell', false);
+
+INSERT INTO book (title, author, isBorrowed) VALUES
+('The Case of the Scarlet Lantern', 'Fiona Aldridge', false);
+
+INSERT INTO book (title, author, isBorrowed) VALUES
+('Murder on Frostfall Ridge', 'Caleb Storme', false);
+
+INSERT INTO book (title, author, isBorrowed) VALUES
+('Secrets of the Forgotten Cellar', 'Nadia Wren', false);
+
+INSERT INTO book (title, author, isBorrowed) VALUES
+('The Last Footprint', 'Dorian Hale', false);
